b84d0caf90e7c2549e96322b36bcac5907819a50
Cong-Thuan Do <congthuan274@gmail.com>
2022-02-28 22:25:32 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Homepage</h1>
+</template>
+
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
