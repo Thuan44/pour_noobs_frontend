c94b8d447e420e598613e6619c2f45ee2fefabc8
Cong-Thuan Do <congthuan274@gmail.com>
2022-02-28 22:25:32 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Homepage</h1>
+  <form action="http://127.0.0.1:8000/logout" method="POST">
+    <button @click="logout">Logout</button>
+  </form>
+</template>
+
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {},
+  setup() {
+    const logout = () => {
+      axios.post()
+    }
+    return {};
+  },
+};
+</script>
