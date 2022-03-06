92c4b6b2ff664107043cbbaed4829cb68b976abd
Cong-Thuan Do <congthuan274@gmail.com>
2022-03-06 17:57:18 +0100

    feat: route for SingleCourse view created

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Register from "@/views/auth/Register.vue";
 import Login from "@/views/auth/Login.vue";
+import SingleCourse from "@/views/SingleCourse.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/courses/:id",
+    name: "SingleCourse",
+    component: SingleCourse,
   },
 ];
 
